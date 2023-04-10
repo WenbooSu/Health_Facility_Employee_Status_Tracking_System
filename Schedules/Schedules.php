0b5664d5cb8f5de50c6733fd47cb3ba1cf12fbf6
WenbooSu <105067670+WenbooSu@users.noreply.github.com>
2023-04-10 14:37:22 -0400

    Update Schedules.php

--- Schedules/Schedules.php
+++ Schedules/Schedules.php
@@ -1,6 +1,9 @@
 <?php
 include "Functions.php";
 echo "<h1>"."Schedules"."</h1>"."<br>";
+echo "<form action='AssignSchedules.php' method='POST'>";
+echo "<input type='submit' name='assign' value='Assign'>";
+echo "</form>";
 $db = getConnect();
 $sql = "SELECT * FROM Schedule";
 $result = $db->query($sql);
