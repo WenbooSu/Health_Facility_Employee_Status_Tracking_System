825f61d94c174da0a77894a1d4c1807a23bcc07c
WenbooSu <105067670+WenbooSu@users.noreply.github.com>
2023-04-10 13:15:37 -0400

    Update EditSchedules.php

--- Schedules/EditSchedules.php
+++ Schedules/EditSchedules.php
@@ -25,17 +25,17 @@ if (isset($_POST['update'])) {
     $date = $_POST['date'];
 
     $sql = "UPDATE Schedule SET start_time = '$start_time', end_time = '$end_time', date = '$date' WHERE EID = '$EID' AND ID = $ID";
+
     //EID 在第一个post的时候丢失了
     //print $sql;
-    echo $sql;
     $db->query($sql);
-    header("Location: Schedules.php");
+   header("Location: Schedules.php");
     exit();
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,22 +45,23 @@ if (isset($_POST['update'])) {
 <body>
 <h1>Edit Schedules</h1>
 <form action="EditSchedules.php" method="POST">
-    <input type="hidden" name="EID" value="<?php echo $EID; ?>">
+    <input type="hidden" name="EID" value="<?php echo isset($fac_data) ? $fac_data['EID'] : ''; ?>">
+    <!--这里的EID获取在MAC和Windows上不一样-->
     <input type="hidden" name="ID" value="<?php echo $ID; ?>">
 
     <label for="start_time">Start Time:</label>
-    <input type="text" id="start_time" name="start_time" placeholder="<?php echo isset($start_time) ? $fac_data['start_time'] : ''; ?>">
+    <input type="text" id="start_time" name="start_time" placeholder="<?php echo isset($fac_data) ? $fac_data['start_time'] : ''; ?>">
     <br><br>
 
     <label for="end_time">End Time::</label>
     <input type="text" id="end_time" name="end_time" placeholder="<?php echo isset($fac_data) ? $fac_data['end_time'] : ''; ?>">
     <br><br>
 
-    <label for="date">End Time::</label>
+    <label for="date">Date::</label>
     <input type="text" id="date" name="date" placeholder="<?php echo isset($fac_data) ? $fac_data['date'] : ''; ?>">
     <br><br>
 
     <input type="submit" name="update" value="Update">
 </form>
 </body>
-</html>
+    </html>
